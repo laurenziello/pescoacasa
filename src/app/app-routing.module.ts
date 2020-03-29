4f99682d48a2824393c2f832431c1ddd8e25275d
cesare <laurenziello@hotmail.it>
2020-03-29 19:39:49 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./root/main/main.module').then(m => m.MainModule)
+  },
+  {
+    path: 'favoledipane',
+    loadChildren: () =>
+      import('./root/favoledipane/favoledipane.module').then(
+        m => m.FavoledipaneModule
+      )
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
